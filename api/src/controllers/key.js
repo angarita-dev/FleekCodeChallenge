d9c41cc6ce13daacfbaa9c01a6f54245163cb683
barita <44899916+angarita-dev@users.noreply.github.com>
2021-01-30 18:51:09 -0500

    Added controllers

---
+++ api/src/controllers/key.js
@@ -0,0 +1,79 @@
+const generateSafeId = require('generate-safe-id');
+// Validators
+const validateRemoveInput = require("../validators/key");
+
+// Models
+const Key = require('../models/key');
+const User = require('../models/user');
+
+// Exports controller methods
+module.exports = {
+  // Creates new key
+  keyCreate: (req, res) => {
+    // Generates secure key
+    const newAPIKEY = generateSafeId();
+
+    // Creates & saves new key
+    const newKey = new Key({
+      key: newAPIKEY,
+      owner: req.user.id
+    })
+      .save()
+      .then(key => {
+        return User.findOneAndUpdate(
+          { _id: req.user.id },
+          { $push: { keys: key._id } }
+        )})
+      .then(newUser => {
+        res.status(201).json(key)
+      })
+      .catch(err => console.log(err));
+  },
+
+  // Deletes key
+  keyDelete: (req, res) => {
+    const { errors, isValid } = validateRemoveInput(req.body);
+
+    // Validations
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    // Finds key to get key id
+    Key.findOne({ key: req.body.key })
+      .then(key => {
+        // Return 400 if key doesn't exists
+        if (!key) {
+          return res.status(400).json({
+            key: "Key doesn't exists"
+          })
+        }
+
+        // Finds User using the _id and key
+        const filter = {
+          _id: req.user.id,
+          keys: mongoose.Types.ObjectId(key._id)
+        }
+
+        // Removes key from keys array
+        const update = {
+          $pull: { keys: key._id }
+        }
+
+        // Find User with listed key
+        return User.findOneAndUpdate(filter, update)
+          .then(user => {
+            // Return 400 if key doesn't belong to User
+            if (!user) {
+              return res.status(400).json({
+                key: "Key doesn't belong to current user"
+              })
+            }
+          })
+          .then(user => Key.findOneAndDelete({ key: req.body.key }))
+      })
+    // Returns deleted key
+      .then(key => res.status(200).json({ deletedKey: key.key }))
+      .catch(err => console.log(err));
+  }
+};
---
+++ api/src/controllers/user.js
@@ -0,0 +1,135 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
+// Validators
+const {
+  validateLoginInput,
+  validateRegisterInput
+} = require("../../src/validators/user");
+
+// Models
+const User = require('../models/user');
+
+module.exports = {
+  // Logins user
+  userLogIn: (req, res) => {
+    // Form validation
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+    User.findOne({ email }).then(user => {
+      // Check if user exists
+      if (!user) {
+        return res.status(404).json({ emailnotfound: "Email not found" });
+      }
+
+      // Check password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User matched
+          // Create JWT Payload
+          const payload = {
+            id: user.id,
+            email: user.email
+          };
+
+          // Sign token
+          jwt.sign(
+            payload,
+            keys.secret,
+            {
+              expiresIn: 31556926 // 1 year in seconds
+            },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: "Bearer " + token
+              });
+            }
+          );
+        } else {
+          return res
+            .status(400)
+            .json({ passwordincorrect: "Password incorrect" });
+        }
+      });
+    });
+  },
+
+  // Creates new User
+  userCreate: (req, res) => {
+    // Form validation
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email }).then(user => {
+      // Validates email uniqueness
+      if (user) {
+        return res.status(400).json({ email: "Email already exists" });
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        // Hash password before saving in database
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                // Create JWT Payload
+                const payload = {
+                  id: user.id,
+                  email: user.email
+                };
+
+                // Sign token
+                jwt.sign(
+                  payload,
+                  keys.secret,
+                  {
+                    expiresIn: 31556926 // 1 year in seconds
+                  },
+                  (err, token) => {
+                    res.json({
+                      success: true,
+                      token: "Bearer " + token
+                    });
+                  }
+                );
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  },
+
+  // Returns keys owned by User
+  userKeys: (req, res) {
+    // Find user by email
+    User.findById(req.user._id)
+      .populate('keys', '-__v')
+      .then(user => res.json({ keys: user.keys }))
+      .catch(err => console.log(err))
+  }
+}
+
+module.exports = router;
