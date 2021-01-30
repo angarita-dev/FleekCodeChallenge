43bee497ba517d749b51d979faf612daf9c928c8
barita <44899916+angarita-dev@users.noreply.github.com>
2021-01-30 11:44:21 -0500

    Added models

--- api/src/models/Key.js
+++ api/src/models/Key.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema; 
 
 const KeySchema = new Schema({
+  key: {
+    type: String,
+    required: true
+  },
+  requestCount: {
+    type: Number,
+    default: 0
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  }
 });
 
 module.exports = Key = mongoose.model('keys', KeySchema);
--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  keys: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'keys'
+    }
+  ]
+});
 
 module.exports = User = mongoose.model("users", UserSchema);
